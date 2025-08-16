9c3b7b4b59a26d6b5f184a342fca2196da7a8ba8
IndieD3v <tanmaymakode76@gmail.com>
2025-08-16 12:10:31 +0530

    fixed server action types - causing issues in build

--- lib/serverAction.ts
+++ lib/serverAction.ts
@@ -3,18 +3,14 @@ import * as z from "zod"
 
 
 type ContactFormState = {
-    success?: boolean;
-    formValues?: {
-        name?: string;
-        email?: string;
-        message?: string;
+    success: boolean;
+    formValues: {
+        name: string;
+        email: string;
+        message: string;
     };
-    formattedError?: {
-        fieldErrors?: {
-            name?: string;
-            email?: string;
-            message?: string;
-        };
+    formattedError: {
+        fieldErrors: Record<string, string[]>;
     };
 };
 
@@ -26,11 +22,11 @@ const formSchema = z.object({
 })
 
 
-export default async function submitContactUsForm(_state: ContactFormState, formData: FormData) {
+export default async function submitContactUsForm(prevState: ContactFormState, formData: FormData): Promise<ContactFormState> {
     const formValues = {
-        name: formData.get('name'),
-        email: formData.get('email'),
-        message: formData.get('message')
+        name: formData.get('name') as string,
+        email: formData.get('email') as string,
+        message: formData.get('message') as string
     }
 
     const data = formSchema.safeParse(formValues)
@@ -38,13 +34,21 @@ export default async function submitContactUsForm(_state: ContactFormState, form
     if (!data.success) {
         const formattedError = z.flattenError(data.error)
 
-        return { formattedError, formValues }
+        return {
+            success: false,
+            formattedError,
+            formValues
+        }
     }
 
     // DO A POST REQUEST HERE
-    // const response = await fetch("https://api.trufflehomes.com/contact-us-form/", { method: 'POST' })
+    // const response = await fetch("https://api.trufflehomes.com/contact-us-form/", { method: 'POST',})
     // const status = await response.json()
 
-    return data
+    return {
+        success: true,
+        formValues: { name: "", email: "", message: "" },
+        formattedError: { fieldErrors: {}}
+    }
 }
 
