0353685dfcb88bbef7be3b20f4cf05e4eb7920d2
IndieD3v <tanmaymakode76@gmail.com>
2025-08-19 10:50:02 +0530

    added og image, timing changes for fadeimage

--- components/FadeImage.tsx
+++ components/FadeImage.tsx
@@ -1,45 +1,3 @@
-// "use client"
-
-// import Image from "next/image";
-// import { useEffect, useState } from "react";
-// import { motion, AnimatePresence } from "motion/react";
-
-
-// export default function FadeImage() {
-//     const [currentIndex, setCurrentIndex] = useState(0);
-
-//     const images = [
-//         '/LandingPage/hero_image.webp',
-//         '/LandingPage/hero_image2.jpg',
-//         '/LandingPage/hero_image3.jpg',
-//         '/LandingPage/hero_image4.jpg',
-//     ]
-
-//     useEffect(() => {
-//         const interval = setInterval(() => {
-//             setCurrentIndex((prevIndex) =>
-//                 prevIndex === images.length - 1 ? 0 : prevIndex + 1
-//             );
-//         }, 3000);
-
-//         return () => clearInterval(interval)
-//     }, [images.length])
-
-//     return (
-//         <AnimatePresence mode="wait">
-//             <motion.div
-//                 key={currentIndex} // ensures AnimatePresence detects change
-//                 initial={{ opacity: 0.5 }}
-//                 animate={{ opacity: 1 }}
-//                 exit={{ opacity: 0.5 }}
-//                 transition={{ duration: .2 }}
-//             >
-//                 <Image src={images[currentIndex]} priority={true} alt="Hero Image" className="max-md:h-[350px] h-[600px] max-md:object-cover object-cover border-2 border-truffle-green-dark rounded-[60px] max-md:rounded-[40px]" width={2134} height={1032} />
-//             </motion.div>
-//         </AnimatePresence >
-//     )
-// }
-
 "use client"
 
 import Image from "next/image";
@@ -62,7 +20,7 @@ export default function FadeImage() {
             setCurrentIndex((prevIndex) =>
                 prevIndex === images.length - 1 ? 0 : prevIndex + 1
             );
-        }, 3000);
+        }, 8000);
 
         return () => clearInterval(interval)
     }, [images.length])
